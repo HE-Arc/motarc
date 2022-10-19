0e6765e7314039a84e89cac8fd317fb726bfbca1
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2022-10-19 16:13:10 +0200

    Adding migrations + controllers

---
+++ database/migrations/2022_10_19_135700_create_ads_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->integer("price");
+            $table->integer("km");
+            $table->float("power_kw");
+            $table->char("color_hexa");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+};
---
+++ database/migrations/2022_10_19_140832_add_model_fk_to_ads.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->foreignId('model_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->dropForeign([
+                "model_id"
+            ]);
+            $table->dropColumn("model_id");
+        });
+    }
+};
