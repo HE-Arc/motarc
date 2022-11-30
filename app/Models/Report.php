75a408ab463e62b0f531fa4482b6b2c1fc8e83fd
Jasmin Saric <jasmin.saric@he-arc.ch>
2022-11-30 15:40:16 +0100

    add report model

---
+++ app/Models/Report.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'ad_id',
+        'message'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ad()
+    {
+        return $this->belongsTo(Ad::class);
+    }
+}
