cb0d4109c38ba743e89c77e50d57b245bd184fbd
Jasmin Saric <jasmin.saric@he-arc.ch>
2022-11-30 15:39:59 +0100

    add report migration

---
+++ database/migrations/2022_11_30_141451_add_foreigns_to_reports.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table)
+        {
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['ad_id']);
+            $table->dropColumn('user_id');
+            $table->dropColumn('ad_id');
+        });
+    }
+};
