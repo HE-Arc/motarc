c3776cef01f220dbf5dff66cebdf6a62e1205110
Jeanne Michel <jeanne.michel@he-arc.ch>
2022-12-16 14:31:13 +0100

    work in progress

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -15,17 +15,8 @@
                 </form>
                 <p class="text-blue-grey-4">Doesn't have an account ? <Link href="/register" id="register-link">Register</Link></p>
             </div>
-<<<<<<< HEAD
-            <q-input v-model="form.email" type="email" id="email" label="E-mail" placeholder="Your e-mail adress" color="primary" label-color="primary"/>
-            <q-input v-model="form.password" type="password" id="password" label="Password" placeholder="Your password" color="primary" label-color="primary"/>
-            <q-btn type="submit" :disabled="form.processing" no-caps color="primary" label="Login" />
-        </form>
-        <p class="text-blue-grey-4">Doesn't have an account ? <Link class="text-orange-9" href="/register">Register</Link></p>
-    </div>
-=======
         </q-card-section>
     </q-card>
->>>>>>> 98f0892 (Fix issues #12, #5, #4)
     <!--</AppLayout>-->
 </template>
 <script>
