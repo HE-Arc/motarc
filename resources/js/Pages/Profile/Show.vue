44d10ad8337af90dadd081a6791fa51f5d9730dd
Jeanne Michel <jeanne.michel@he-arc.ch>
2022-12-19 13:55:46 +0100

    Beautiful profile page

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -1,27 +1,56 @@
 <template>
+    <div class="row justify-center items-center ">
+        <div class="col-xs-12 col-md-6 q-mt-xl">
     <q-card>
         <q-card-section>
+            <div class="q-mx-md">
             <h2>My profile</h2>
 
-            <q-item-label caption class="text-weight-bold"> Name </q-item-label>
-            <q-item-label caption>{{ user.name }}</q-item-label>
+            <div class="row items-center">
+            <div class="col-2">
+                <div class="row q-ma-sm q-mb-md">
+                    <q-icon name="person" class="q-mr-sm"/>
+                    <q-item-label class="text-weight-bold"> Name </q-item-label>
+                </div>
+                <div class="row q-ma-sm q-mb-md">
+                    <q-icon name="email" class="q-mr-sm" />
+                    <q-item-label class="text-weight-bold"> Email </q-item-label>
+                </div>
+                <div class="row q-ma-sm q-mb-md">
+                    <q-icon name="location_on" class="q-mr-sm" />
+                    <q-item-label class="text-weight-bold"> NPA </q-item-label>
+                </div>
+            </div>
 
-            <q-item-label caption class="text-weight-bold"> Email </q-item-label>
-            <q-item-label caption>{{ user.email }}</q-item-label>
+            <div class="col-3">
+                <div class="row q-ma-sm q-mb-md">
+                    <q-item-label caption>{{ user.name }}</q-item-label>
+                </div>
+                <div class="row q-ma-sm q-mb-md">
+                    <q-item-label caption>{{ user.email }}</q-item-label>
+                </div>
+                <div class="row q-ma-sm q-mb-md">
+                    <q-item-label caption>{{ user.npa }}</q-item-label>
+                </div>
+            </div>
 
-            <q-item-label caption class="text-weight-bold"> NPA </q-item-label>
-            <q-item-label caption>{{ user.npa }}</q-item-label>
+            </div>
 
             <!-- Button modify -->
+            <Link href="/profile/edit">
             <q-btn
-                class="q-mt-md"
+                class="q-my-md"
                 color="primary"
                 to="/profile/edit"
+                label="Modify"
             >
-            <Link href="/profile/edit">Modify</Link>
             </q-btn>
+        </Link>
+        </div>
         </q-card-section>
     </q-card>
+    </div>
+</div>
 </template>
 
 <script>
