23bef5edda8da8fe91510b77df52d8132404be7f
Jeanne Michel <jeanne.michel@he-arc.ch>
2022-12-19 15:50:57 +0100

    Beautiful myAds page

--- resources/js/Pages/Ads/MyAds.vue
+++ resources/js/Pages/Ads/MyAds.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="row justify-center items-center">
-    <div class="col-6 ">
+    <div class="col-7">
         <h2>My ads</h2>
 
         <!-- If no ads, print message -->
@@ -8,30 +8,33 @@
             <p>You have no ads yet :(</p>
         </div>
 
-        <q-card v-for="ad in ads" :key="ad.id">
-            <q-card-section>
-                <div class="row">
-                <div class="col-6">
-                <img v-if="ad.images[0] !== undefined" :src="'/storage/images/' + ad.images[0].image_url" width="200" />
-                </div>
-                <div class="col-6">
-                <h3>{{ ad.model.brand + " " + ad.model.model }}</h3>
+        <q-card v-for="ad in ads" :key="ad.id" class="q-my-sm">
+            <q-card-section horizontal>
+                <img  v-if="ad.images[0] !== undefined" fit="fill" class="col-5" :src="'/storage/images/' + ad.images[0].image_url" />
+                <img v-else class="col-4" fit="fill" src="/storage/images/moto_base.png" />
+
+                <q-card-section>
+                <div class="col-8">
+
+                    <h3 class="q-my-md">{{ ad.model.brand + " " + ad.model.model }}</h3>
 
                 <!--<div class="row-items">-->
-                <div class="row">
-                    <q-item-label caption class="text-weight-bold"> KM </q-item-label>
-                    <q-item-label caption>{{ ad.km }}</q-item-label>
-                    <q-item-label caption class="text-weight-bold"> Year </q-item-label>
-                    <q-item-label caption>{{ ad.model.year }}</q-item-label>
-                </div>
+                    <div class="row q-my-sm">
+                        <!-- Grey text -->
+                        <p class="text-grey-8 q-mr-md">{{ ad.km }} km </p>
+                        <q-icon color="grey-8" name="event" />
+                        <p class="text-grey-8 q-mr-md"> {{ ad.model.year }} </p>
+                        <q-icon color="grey-8" name="bolt"></q-icon>
+                        <p class="text-grey-8 ">{{ ad.power_kw }} kw </p>
+                    </div>
 
-                <h4>{{ad.price}}.-</h4>
+                    <h4 class="q-my-sm">{{ad.price}}.-</h4>
 
                 <!-- Button modify -->
-                    <q-btn class="q-mt-md" color="primary" icon="edit" @click="modifyAd(ad.id)">Edit</q-btn>
+                    <q-btn class="q-my-md" color="primary" icon="edit" @click="modifyAd(ad.id)">Edit</q-btn>
                 <!--</div>-->
                 </div>
-            </div>
+                </q-card-section>
             </q-card-section>
         </q-card>
     </div>
